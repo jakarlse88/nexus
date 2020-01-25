086e0249b7f2738868432dee5ef91caab13c22bc
Jon Karlsen <karlsen.jonarild@gmail.com>
2020-01-25 23:20:05 +0100

    Landing section first draft

---
+++ Nexus/Components/Hero/HeroBase.razor.cs
@@ -0,0 +1,30 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Nexus.Components 
+{
+    public class HeroBase : ComponentBase
+    {
+        private readonly IJSRuntime _jsRuntime;
+        public ElementReference _particles { get; set; }
+        public string Title = "Jon Karlsen";
+        public string Subtitle = "Developer // Black Belt";
+
+        public HeroBase()
+        {
+        }
+
+        public HeroBase(IJSRuntime jsRuntime)
+        {
+            _jsRuntime = jsRuntime;
+        }
+
+        // protected override async Task OnAfterRenderAsync(bool firstRender){
+        //     if (firstRender)
+        //     {
+        //         await _jsRuntime.InvokeVoidAsync("particlesLoad", _particles);
+        //     }
+        // }
+    }
+}
