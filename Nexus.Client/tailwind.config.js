39a676204fa35166564424869fc0fc3d019c812e
Jon Karlsen <karlsen.jonarild@gmail.com>
2020-02-01 23:36:25 +0100

    TailwindCSS
    Fix some fontfamilies apparently broken by adding a custom family

--- Nexus.Client/tailwind.config.js
+++ Nexus.Client/tailwind.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   theme: {
     fontFamily: {
-      "handwriting" : ["Caveat", "cursive"]
+      "handwriting" : ["Caveat", "cursive"],
+      "mono": ["Menlo", "Monaco", "Consolas", "Liberation\\ Mono", "Courier\\ New", "monospace"],
+      "sans": ["system-ui", "BlinkMacSystemFont", "-apple-system", "Segoe\\ UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira\\ Sans", "Droid\\ Sans", "Helvetica\\ Neue", "sans-serif"]
     },
     extend: {}
   },
