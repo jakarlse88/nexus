4d821907832490afca9265cf3f11801e1801fd31
Jon Karlsen <karlsen.jonarild@gmail.com>
2020-02-13 22:09:23 +0100

    feature/work-section
    Prototype structure

---
+++ Nexus.Client/Components/WorkSection/WorkSection.razor.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+
+namespace Nexus.Client.Components.WorkSection
+{
+    public class WorkSectionBase : ComponentBase
+    {
+    }
+}
