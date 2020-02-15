e199d1ab5788c0b5ec644016b4f5a86115de660a
Jon Karlsen <karlsen.jonarild@gmail.com>
2020-02-16 00:57:25 +0100

    feature: test-project
    Add tests

---
+++ Nexus/Nexus.Test/LocalFileServiceTests.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Nexus.Server.Services;
+using Xunit;
+
+namespace Nexus.Test
+{
+    public class LocalFileServiceTests
+    {
+        [Fact]
+        public async Task TestRetrieveLocalFileStreamFileNameInvalid()
+        {
+            // Arrange
+            const string fileName = "invalid.pdf";
+
+            var localFileService = new LocalFileService();
+            
+            // Act
+            var result = 
+                await  localFileService.RetrieveLocalFileStream(fileName);
+
+            // Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async Task TestRetrieveLocalFileStreamFileNameValid()
+        {
+            // Arrange
+            const string fileName = "resume.pdf";
+
+            var localFileService = new LocalFileService();
+            
+            // Act
+            var result = 
+                await  localFileService.RetrieveLocalFileStream(fileName);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsAssignableFrom<FileStream>(result);
+        }
+
+    }
+}
---
+++ Nexus/Nexus.Test/TestUtilities.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Nexus.Server.Models;
+
+namespace Nexus.Test
+{
+    public static class TestUtilities
+    {
+        internal static DbContextOptions<NexusContext> BuildTestDbOptions()
+        {
+            return new DbContextOptionsBuilder<NexusContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+        }
+
+        public static void SeedTestDb(NexusContext context)
+        {
+            context.PersonalDetails.Add(
+                new PersonalDetails
+                {
+                    Id = 1,
+                    FirstName = "Jon",
+                    LastName = "Karlsen",
+                    Biography = "Once upon a time",
+                    JobTitle = "Back-End Web Dev"
+                });
+
+            context.SaveChanges();
+        }
+    }
+}
